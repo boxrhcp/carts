6d96b357b63f3b62713a8baa76820aafc1a5b008
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-08-12 12:10:16 +0200

    force shutdown

--- src/main/java/works/weave/socks/cart/CartApplication.java
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -1,13 +1,25 @@
 package works.weave.socks.cart;
 
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnablePrometheusEndpoint
 public class CartApplication {
+    private static final Logger logger = LoggerFactory.getLogger("Main");
+    static ConfigurableApplicationContext ctx;
     public static void main(String[] args) {
-        SpringApplication.run(CartApplication.class, args);
+        ctx = SpringApplication.run(CartApplication.class, args);
+    }
+
+    public static void exitApplication() {
+        logger.error("CLOSING APP!");
+        int returnCode = -1;
+        int exitCode = SpringApplication.exit(ctx, () -> returnCode);
+        System.exit(exitCode);
     }
 }
--- src/main/java/works/weave/socks/cart/controllers/CartsController.java
+++ src/main/java/works/weave/socks/cart/controllers/CartsController.java
@@ -3,12 +3,16 @@ package works.weave.socks.cart.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import works.weave.socks.cart.CartApplication;
 import works.weave.socks.cart.cart.CartDAO;
 import works.weave.socks.cart.cart.CartResource;
 import works.weave.socks.cart.entities.Cart;
+
+import java.io.IOException;
 
 
 @RestController
@@ -23,6 +27,13 @@ public class CartsController {
     @RequestMapping(value = "/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public Cart get(@PathVariable String customerId) {
         return new CartResource(cartDAO, customerId).value().get();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public void get() {
+        logger.info("CLOSING APP!");
+        CartApplication.exitApplication();
     }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
